aa4b1249e330938f44c52feeec3c372175039fbc
CaioFML <caio.felipemotta@hotmail.com>
2021-10-11 18:02:28 -0300

    Add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery";
+import "bootstrap"
+// import "bootstrap/dist/js/bootstrap";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require.context("../images", true);
+
+window.jQuery = $;
+window.$ = $;
