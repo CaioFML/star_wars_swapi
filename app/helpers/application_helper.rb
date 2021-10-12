4a6146c046eeffe7578bbf9a19b04cbf0b02918c
CaioFML <caio.felipemotta@hotmail.com>
2021-10-11 23:59:21 -0300

    Fix offenses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,9 @@ module ApplicationHelper
   include Webpacker::Helper
 
   def character_image(person)
-    return image_tag(person.image_url, class: "card-img", height: "500") if person.image_url.present?
+    image_url = person.image_url
+
+    return image_tag(image_url, class: "card-img", height: "500") if image_url.present?
 
     image_pack_tag("unknown_character.png", class: "card-img", height: "500")
   end
