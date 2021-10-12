dad501c65659af8b039e2291493240302a39d257
CaioFML <caio.felipemotta@hotmail.com>
2021-10-11 23:43:33 -0300

    Add show

--- spec/system/people_spec.rb
+++ spec/system/people_spec.rb
@@ -8,9 +8,9 @@ describe "People" do
 
     let(:people) do
       [
-        create(:person, name: "Luke Skywalker"),
-        create(:person, name: "Darth Vader"),
-        create(:person, name: "Leia Organa")
+        create(:person, name: "Luke Skywalker", image_url: "https://url1.com"),
+        create(:person, name: "Darth Vader", image_url: "https://url2.com"),
+        create(:person, name: "Leia Organa", image_url: "https://url3.com")
       ]
     end
 
@@ -21,12 +21,44 @@ describe "People" do
     it "displays card info" do
       expect(page).to have_selector("a[href='#{person_path(people.first)}']")
       expect(page).to have_selector("div.card-img-overlay h5.card-title", text: "Luke Skywalker")
+      expect(page).to have_selector("div.card img[src='https://url1.com']")
 
       expect(page).to have_selector("a[href='#{person_path(people.second)}']")
       expect(page).to have_selector("div.card-img-overlay h5.card-title", text: "Darth Vader")
+      expect(page).to have_selector("div.card img[src='https://url2.com']")
 
       expect(page).to have_selector("a[href='#{person_path(people.third)}']")
       expect(page).to have_selector("div.card-img-overlay h5.card-title", text: "Leia Organa")
+      expect(page).to have_selector("div.card img[src='https://url3.com']")
+    end
+  end
+
+  describe "show" do
+    before { visit person_path(person) }
+
+    let(:person) do
+      create(:person,
+             name: "Anakin",
+             image_url: "https://url1.com",
+             height: "188",
+             mass: "84",
+             homeworld: create(:planet, name: "Tatooine"),
+             birth_year: "41.9BBY",
+             species: [create(:specie, name: "Human")],
+             starships: [create(:starship, name: "Jedi interceptor")])
+    end
+
+    it "displays back button" do
+      expect(page).to have_link "<< Back", href: people_path
+    end
+
+    it "displays card info" do
+      expect(page).to have_selector("div.col-md-4.bg-dark img[src='https://url1.com']")
+      expect(page).to have_selector("h5.card-title", text: "Anakin")
+      expect(page)
+        .to have_selector("p.card-text", text: "Hello! I'm Anakin. I'm a Human, my weight is 84 kg and my height "\
+                                               "is 188 cm. My homeworld is Tatooine, I was born in the year 41.9BBY "\
+                                               "and I've piloted Jedi interceptor.")
     end
   end
 end
